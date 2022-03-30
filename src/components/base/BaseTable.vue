7bdc6912174f8f3260ce1d65221b6e540daae63d
Jonathan CIERP <cierp.jonathan@gmail.com>
2022-03-30 23:52:53 +0200

    Feat(components) : Add BaseTable & BaseFormInput fix model value props

---
+++ src/components/base/BaseTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th
+          v-for="item in items"
+          :key="item"
+          class="p-4 border border-gray-300 whitespace-nowrap"
+        >
+          {{ item.key }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td
+          v-for="item in items"
+          :key="item"
+          class="p-4 border border-gray-300 whitespace-nowrap"
+          :class="[item.isDivisibleBy3 ? 'font-bold' : '']"
+        >
+          {{ item.value }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
--- src/components/base/form/BaseFormInput.vue
+++ src/components/base/form/BaseFormInput.vue
@@ -4,14 +4,18 @@
       {{ label }}
     </label>
     <input
+      v-model="value"
       :type="type"
-      class="mt-1 py-2 px-2 focus:ring-indigo-500 focus:border-indigo-500 block w-64 shadow-sm border-gray-800 border rounded-md"
+      class="mt-1 py-2 px-2 focus:ring-indigo-500 focus:border-indigo-500 block w-32 shadow-sm border-gray-800 border rounded-md"
+      @update:model-value="$emit('update:modelValue', value)"
     />
   </div>
 </template>
 
 <script setup>
-defineProps({
+import { ref } from 'vue'
+
+const props = defineProps({
   label: {
     type: String,
     default: '',
@@ -20,5 +24,12 @@ defineProps({
     type: String,
     default: 'text',
   },
+  modelValue: {
+    type: [String, Number],
+    default: '',
+  },
 })
+defineEmits(['update:modelValue'])
+
+const value = ref(props.modelValue)
 </script>
