af8a4278deb094555afb266d3a36d814940ce84b
Jonathan CIERP <cierp.jonathan@gmail.com>
2022-03-30 22:45:17 +0200

    Feat(component) : BaseFormInput component

---
+++ src/components/base/form/BaseFormInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <label class="block text-sm font-medium text-gray-700">
+      {{ label }}
+    </label>
+    <input
+      :type="type"
+      class="mt-1 py-2 px-2 focus:ring-indigo-500 focus:border-indigo-500 block w-64 shadow-sm sm:text-sm border-gray-800 border rounded-md"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  label: {
+    type: String,
+    default: '',
+  },
+  type: {
+    type: String,
+    default: 'text',
+  },
+})
+</script>
