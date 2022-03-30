738b04f73cf6205822008b19031ce67fb974bab5
Jonathan CIERP <cierp.jonathan@gmail.com>
2022-03-30 22:54:17 +0200

    Fix(component) : BaseFormInput classes

--- src/components/base/form/BaseFormInput.vue
+++ src/components/base/form/BaseFormInput.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <label class="block text-sm font-medium text-gray-700">
+    <label class="block font-medium text-gray-700">
       {{ label }}
     </label>
     <input
       :type="type"
-      class="mt-1 py-2 px-2 focus:ring-indigo-500 focus:border-indigo-500 block w-64 shadow-sm sm:text-sm border-gray-800 border rounded-md"
+      class="mt-1 py-2 px-2 focus:ring-indigo-500 focus:border-indigo-500 block w-64 shadow-sm border-gray-800 border rounded-md"
     />
   </div>
 </template>
