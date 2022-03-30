2f44817b5a3c6c0ec58abb39889d173d94a17aae
Jonathan CIERP <cierp.jonathan@gmail.com>
2022-03-30 22:44:53 +0200

    Feat(home) : Explication & home page

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
-<script setup></script>
+<template>
+  <main class="m-8">
+    <h1 class="text-3xl font-bold">Calcul de la suite de Fibonacci</h1>
+    <div class="mb-8">
+      <h3 class="text-xl font-medium">Consignes :</h3>
+      <ul class="ml-8 mt-2">
+        <li class="list-disc">Demander a l’utilisateur un nombre</li>
+        <li class="list-disc">
+          Afficher la suite de Fibonacci jusqu’à ce nombre exclu
+        </li>
+        <li class="list-disc">
+          Et tout les multiples de 3 doivent mis en gras
+        </li>
+      </ul>
+      <p class="mt-2">Le tout en web app avec les techno de ton choix.</p>
+    </div>
+    <form>
+      <BaseFormInput label="Entrez un nombre" type="number" />
+    </form>
+  </main>
+</template>
 
-<template>a</template>
+<script setup>
+import BaseFormInput from '@/components/base/form/BaseFormInput.vue'
+</script>
